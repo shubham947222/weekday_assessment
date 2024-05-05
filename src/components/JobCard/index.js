b9405d90f5c6076f668377846b1219188ba3b5d9
k.shubham <shubham947222@gmail.com>
2024-05-05 19:16:51 +0530

    width set in modal

--- src/components/JobCard/index.js
+++ src/components/JobCard/index.js
@@ -13,6 +13,7 @@ const JobCard = ({ each }) => {
     left: "50%",
     transform: "translate(-50%, -50%)",
     width: 400,
+    maxWidth: "80%",
     bgcolor: "background.paper",
     boxShadow: 24,
     p: 4,
