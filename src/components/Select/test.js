fb5147e69810fb46c8b7fb7a84bd73aac16c1e40
k.shubham <shubham947222@gmail.com>
2024-05-06 11:51:28 +0530

    @mui styles remove

--- src/components/Select/test.js
+++ src/components/Select/test.js
@@ -1,5 +1,4 @@
 import React from "react";
-// import { makeStyles } from "@mui/styles"; // Import makeStyles from @mui/styles
 
 import Select from "@mui/material/Select";
 import Chip from "@mui/material/Chip";
@@ -8,33 +7,11 @@ import MenuItem from "@mui/material/MenuItem";
 import FormControl from "@mui/material/FormControl";
 import { Box } from "@mui/system"; // Import Box from @mui/system
 import OutlinedInput from "@mui/material/OutlinedInput";
-import { makeStyles } from "@mui/styles";
-import theme from "../../utils/theme";
 import { Button } from "@mui/material";
-const useStyles = makeStyles((theme) => ({
-  root: {
-    display: "flex",
-    flexWrap: "wrap",
-  },
-  formControl: {
-    // margin: theme?.spacing(1),
-    margin: 50,
-    minWidth: 420,
-    maxWidth: 600,
-  },
-  chips: {
-    display: "flex",
-    flexWrap: "wrap",
-  },
-  chip: {
-    margin: 2,
-  },
-}));
 
 const names = ["Frontend", "Backend", "Database"];
 
 export default function MultiSelectChip() {
-  const classes = useStyles();
   const [personName, setPersonName] = React.useState([]);
 
   const handleChange = (event) => {
@@ -42,8 +19,8 @@ export default function MultiSelectChip() {
   };
 
   return (
-    <div className={classes.root}>
-      <FormControl className={classes.formControl}>
+    <div>
+      <FormControl>
         <InputLabel id="demo-multiple-chip-label">Select</InputLabel>
         <Select
           labelId="demo-multiple-chip-label"
@@ -55,7 +32,7 @@ export default function MultiSelectChip() {
           renderValue={(selected) => (
             <Box sx={{ display: "flex", flexWrap: "wrap", gap: 0.5 }}>
               {selected.map((value) => (
-                <Chip key={value} label={value} className={classes.chip} />
+                <Chip key={value} label={value} />
               ))}
             </Box>
           )}
